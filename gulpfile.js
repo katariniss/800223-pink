e37035ebe65dbc65fa7b87abc4852bfc2d81accc
Katariniss <echekina@gmail.com>
2018-11-11 21:33:31 +0500

    Исправляет логотип в хедере для трех устройств

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ gulp.task("css", function () {
 });
 
 gulp.task("sprite", function () {
-  return gulp.src("source/img/icon-*.svg")
+  return gulp.src("source/img/*.svg")
     .pipe(svgstore({
       inlineSvg: true
     }))
